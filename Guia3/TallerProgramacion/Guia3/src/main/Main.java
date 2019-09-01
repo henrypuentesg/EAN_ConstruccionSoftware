da9759421c405f55ac9a11ee40b91ad3ab55e2b1
henrypuentesg <henry.puentesg@hotmail.com>
2019-09-01 14:18:17 -0500

    Se envia ejercicio 0, Serie ejericicios de programacion No. 4.

---
+++ Guia3/TallerProgramacion/Guia3/src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import ejercicio00.Pendulo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Pendulo pendulo = new Pendulo(7.10, 9.81);
+		pendulo.periodoOscilacion();
+		
+	}	
+	
+	
+	
+}
